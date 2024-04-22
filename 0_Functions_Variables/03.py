1128ee0dd424e1d43c838eb9f1dc3ad26573d300
Daniel Moscardini <moscardinibdaniel@gmail.com>
2024-04-22 19:13:13 -0300

    Starting with numbers, integers and floats

---
+++ 0_Functions_Variables/03.py
@@ -0,0 +1,4 @@
+# Mathematics on Python
+x = int(input('Whats x? '))
+y = int(input('Whats y? '))
+print(f'Sum: {x + y}')
---
+++ 0_Functions_Variables/04.py
@@ -0,0 +1,5 @@
+# Mathematics on Python
+x = float(input('Whats x? '))
+y = float(input('Whats y? '))
+print(f'Sum: {x + y}')
+print(f'Sum: {(x + y):.2f}')
