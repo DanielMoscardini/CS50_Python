43c3ad5b7d6701329d96453c600643c69ccd71bf
Daniel Moscardini <moscardinibdaniel@gmail.com>
2024-04-22 20:22:48 -0300

    Starting with conditions

---
+++ 1_Conditionals/01.py
@@ -0,0 +1,10 @@
+# Comparing numbers:
+x = int(input('Whats x? '))
+y = int(input('Whats y? '))
+
+if x < y:
+    print('x is less than y')
+if x > y:
+    print('x is greater than y')
+if x == y:
+    print('x is equal than y')
---
+++ 1_Conditionals/02.py
@@ -0,0 +1,10 @@
+x = int(input('Whats x? '))
+y = int(input('Whats y? '))
+
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+elif x == y:
+    print('x is equal to y')
+    
