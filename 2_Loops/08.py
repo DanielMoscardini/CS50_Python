7a4ffbb6cc4fb7a7bb83abcb966e2b98b5d1b5d4
Daniel Moscardini <moscardinibdaniel@gmail.com>
2024-05-09 00:34:57 -0300

    Ending dictionarys

---
+++ 2_Loops/08.py
@@ -0,0 +1,9 @@
+fellowship = {
+    'Frodo': 'Hobbit',
+    'Gandalf': 'Maia',
+    'Aragorn': 'Numenorian',
+    'Legolas': 'Forest Elf'
+}
+
+for name in fellowship:
+    print(f'{name} - {fellowship[name]}')
---
+++ 2_Loops/09.py
@@ -0,0 +1,7 @@
+fellowship = [
+    {'Frodo': 'Hobbit', 'Weapon': 'Sting'},
+    {'Gandalf': 'Maia', 'Weapon': 'White Staff'},
+    {'Aragorn': 'Numenorian', 'Weapon': 'Anduril'},
+    {'Legolas': 'Forest Elf', 'Weapon': 'Lothlorien Bow'}
+]
+print(fellowship[0]['Frodo'])
